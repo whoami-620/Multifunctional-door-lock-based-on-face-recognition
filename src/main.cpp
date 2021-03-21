5633ee11f3802bb7d665e48cdf5d196dfa04d1ad
chenyinzhijin <972812328@qq.com>
2021-03-21 14:56:10 +0800

    add main function and some other files

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Edited by Wenyi Li 
+//
+#include "Face_recognition.hpp"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+Detection();
+}
