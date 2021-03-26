e4dde3b3909a5486fbccaeb551f1c8ac79a73615
chenyinzhijin <972812328@qq.com>
2021-03-27 00:24:01 +0800

    add button function

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,16 @@
 #include "Face_recognition.hpp"
 #include <stdio.h>
 #include <iostream>
+#include "wiringPi.h"
+
+
 
 using namespace std;
 using namespace cv;
 
+
 int main() {
 
-Detection();
+		Detection();
+
 }
