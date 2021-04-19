32bf35370b7be3722e215173f08022a61cae8203
chenyinzhijin <972812328@qq.com>
2021-04-20 00:19:18 +0800

    Add files via upload

---
+++ src/lock.h
@@ -0,0 +1,12 @@
+//
+// Created by fanzou
+//
+
+#ifndef LOCK_H
+#define LOCK_H
+
+int Open();// open the door
+int Close();// close the door
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//
+// Edited by Wenyi Li 
+//
+#include "Face_recognition.hpp"
+#include <stdio.h>
+#include <iostream>
+#include "wiringPi.h"
+
+
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+
+		Detection();
+
+}
